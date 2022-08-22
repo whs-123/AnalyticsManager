24f584776fba0dc44af3eefc7e5fe49c9a909aac
white <baibin@51yund.com>
2022-08-22 12:37:40 +0800

    修改

--- AnalyticsManager/Classes/AnalyticsManager.swift
+++ AnalyticsManager/Classes/AnalyticsManager.swift
@@ -6,37 +6,20 @@
 //
 
 import Foundation
+import UIKit
 import SensorsAnalyticsSDK
 
-public struct AnalyticsConfigOptions {
-    
-    ///触发flush
-    public var flushBulkSize: Int = 100
-    
-    ///最大可以缓存事件
-    public var maxCacheSize: Int = 10000
+public class AnalyticsConfigOptions: SAConfigOptions {
     
     ///是否自动采集时间 点击，启动，启动结束，pv
     public var enableAutoTrack: Bool = false
-    
-    ///是否开启log
-    public var enableLog: Bool = false
-    
-    fileprivate var serverURL: String
-    fileprivate var launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-    
-    public init(serverURL: String, launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) {
-        self.serverURL = serverURL
-        self.launchOptions = launchOptions
-    }
 }
 
 public struct AnalyticsManager {
     
     ///启动采集系统
     public static func startAnalytics(with config: AnalyticsConfigOptions) {
-        let options = SAConfigOptions(serverURL: config.serverURL, launchOptions: config.launchOptions)
-        options.maxCacheSize = config.maxCacheSize;
+        let options = config
         if config.enableAutoTrack {
             options.autoTrackEventType = [.eventTypeAppClick, .eventTypeAppStart, .eventTypeAppEnd, .eventTypeAppViewScreen]
         } else {
@@ -45,8 +28,15 @@ public struct AnalyticsManager {
         
         options.enableVisualizedAutoTrack = true
         options.enableHeatMap = true
-        options.enableLog = config.enableLog
         options.flushNetworkPolicy = .typeALL
         SensorsAnalyticsSDK.start(configOptions: options)
+    }
+    
+    public static func ignoreAutoTrackViewControllers(controllers: [String]) {
+        SensorsAnalyticsSDK.sharedInstance()?.ignoreAutoTrackViewControllers(controllers)
+    }
+    
+    public static func ignoreViewType(viewType: AnyClass) {
+        SensorsAnalyticsSDK.sharedInstance()?.ignoreViewType(viewType)
     }
 }
